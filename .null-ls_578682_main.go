b44570114544d5600f71d707a55230d3be626d35
djaap <djaap@us.ci.org>
2024-02-06 17:01:57 -0700

    init

---
+++ .null-ls_578682_main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+
+	"golang.org/x/net/websocket"
+)
+
+type Server struct {
+	conns map[*websocket.Conn]bool
+}
+
+func NewServer() *Server {
+	return &Server{
+		conns: make(map[*websocket.Conn]bool),
+	}
+}
+
+func (s *Server) handleWS(ws *websocket.Conn) {
+	fmt.Println("new incoming connection from client:", ws.RemoteAddr())
+
+}
+
+func main() {
+
+}
